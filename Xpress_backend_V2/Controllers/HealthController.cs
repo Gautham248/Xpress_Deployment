66b53b0aebcd199b3fa2140d23e8e35908dedb5c
Gautham Krishna N <gkn240801@gmail.com>
2025-06-13 15:25:53 +0530

    changes to dockerize

---
+++ Xpress_backend_V2/Controllers/HealthController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Xpress_backend_V2.Controllers
+{
+    [Route("/")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get() => Ok("Xpress backend API is live 🚀");
+    }
+
+}
